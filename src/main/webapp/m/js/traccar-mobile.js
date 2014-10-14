6c182f55846f1ae48a10c365bae936648b762213
Vitaly Litvak <vitavaque@gmail.com>
2014-10-14 10:28:15 +0400

    Added map view based on OpenLayers 3

--- src/main/webapp/m/js/traccar-mobile.js
+++ src/main/webapp/m/js/traccar-mobile.js
@@ -11,17 +11,34 @@ var $$ = Dom7;
 // Add view
 var mainView = myApp.addView('.view-main');
 
+// Application state, user settings, devices, etc.
+var appState = {};
+
 // check authentication
 callGet({ method: 'authenticated',
-          success: function() { mainView.loadPage({url: 'pages/map.html', animatePages: false}); },
+          success: function(data) {
+              // save user and his settings to the application state
+              appState.user = data;
+              appState.userSettings = data.userSettings;
+              mainView.loadPage({url: 'pages/map.html', animatePages: false});
+          },
           error: function() { mainView.loadPage({url: 'pages/login.html', animatePages: false}); }
         });
 
 // set up logout action
 $$('#logout').on('click', function() {
     callGet({ method: 'logout',
-        success: function() { myApp.closePanel(); mainView.loadPage('pages/login.html');},
-        error: function() { myApp.alert("Unexpected error"); mainView.loadPage('pages/login.html'); }})
+        success: function() {
+            myApp.closePanel();
+            mainView.loadPage('pages/login.html');
+            appState = {};
+            $$('#map').html('');
+        },
+        error: function() {
+            myApp.alert("Unexpected error");
+            mainView.loadPage('pages/login.html');
+        }
+    });
 });
 
 myApp.onPageInit('login-screen', function (page) {
@@ -41,17 +58,45 @@ myApp.onPageInit('login-screen', function (page) {
 
         callPost({ method: 'login',
                    data: [username, password],
-                   success: function() { mainView.loadPage('pages/map.html'); },
+                   success: function(data) {
+                       // save user and his settings to the application state
+                       appState.user = data;
+                       appState.userSettings = data.userSettings;
+
+                       mainView.loadPage('pages/map.html');
+                   },
                    error: function() { myApp.alert("User name or password is invalid"); },
                    showIndicator: true });
     });
 });
 
+// initialize map when page ready
+var map;
+
 myApp.onPageInit('map-screen', function(page) {
     myApp.params.swipePanel = 'left';
 
     loadDevices();
+
+    map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ]
+    });
+
+    // set up map center and zoom
+    map.setView(new ol.View({
+        center: createPoint(appState.userSettings.centerLongitude, appState.userSettings.centerLatitude),
+        zoom: appState.userSettings.zoomLevel
+    }));
 });
+
+function createPoint(lon, lat) {
+    return ol.proj.transform([lon, lat], 'EPSG:4326', map.getView().getProjection());
+}
 
 function loadDevices() {
     var devicesList = $$('#devicesList');
@@ -59,6 +104,9 @@ function loadDevices() {
 
     callGet({ method: 'getDevices',
         success: function(data) {
+            // save devices into application state
+            appState.devices = data;
+
             var listHTML = '<div class="list-block">';
             listHTML += '<div class="list-block-label">Devices</div>';
             listHTML += '<ul>';
