491ddea4683f7f1f04edae54dc7d7d13b6e414ea
Vitaly Litvak <vitavaque@gmail.com>
2015-04-30 08:50:42 +0300

    Corrected equals() and hashCode() methods of Maintenance entity to work with hibernate proxies

--- src/main/java/org/traccar/web/shared/model/Maintenance.java
+++ src/main/java/org/traccar/web/shared/model/Maintenance.java
@@ -96,24 +96,24 @@ public class Maintenance implements Serializable {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Maintenance)) return false;
 
         Maintenance that = (Maintenance) o;
 
-        if (id != that.id) return false;
-        if (indexNo != that.indexNo) return false;
-        if (device != null ? !device.equals(that.device) : that.device != null) return false;
-        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (getId() != that.getId()) return false;
+        if (getIndexNo() != that.getIndexNo()) return false;
+        if (getDevice() != null ? !getDevice().equals(that.getDevice()) : that.getDevice() != null) return false;
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
 
         return true;
     }
 
     @Override
     public int hashCode() {
-        int result = (int) (id ^ (id >>> 32));
-        result = 31 * result + (device != null ? device.hashCode() : 0);
-        result = 31 * result + indexNo;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
+        int result = (int) (getId() ^ (getId() >>> 32));
+        result = 31 * result + (getDevice() != null ? getDevice().hashCode() : 0);
+        result = 31 * result + getIndexNo();
+        result = 31 * result + (getName() != null ? getName().hashCode() : 0);
         return result;
     }
 
