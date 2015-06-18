32137e7fdce8f83bbbb412b649f6574cf0ec3365
mpele <pmilanovic@gmail.com>
2015-06-18 20:12:07 +0200

    Serbian translation

--- src/main/webapp/m/js/i18n/Messages.js
+++ src/main/webapp/m/js/i18n/Messages.js
@@ -1,4 +1,38 @@
 var i18n = {
+	    rs: {
+	        _name_: 'Srpski',
+	        // login screen
+	        language: 'Jezik',
+	        username: 'Username',
+	        password: 'Password',
+	        your_username: 'Your username',
+	        your_password: 'Your password',
+	        sign_in: 'Sign In',
+	        user_name_and_password_must_not_be_empty: 'User name and password must not be empty',
+	        user_name_or_password_is_invalid: 'User name or password is invalid',
+	        unexpected_error: 'Unexpected error',
+	        open_desktop_version: 'Open Desktop version',
+	        // sidebar
+	        devices: 'Devices',
+	        logout: 'Logout',
+	        // device details
+	        select_on_map: 'Select on map',
+	        no_data_available: 'No data available',
+	        time: 'Time',
+	        latitude: 'Latitude',
+	        longitude: 'Longitude',
+	        altitude: 'Altitude',
+	        speed: 'Speed',
+	        course: 'Course',
+	        address: 'Address',
+	        follow: 'Follow',
+	        stop_following: 'Stop following',
+	        geo_fence: 'Geo-fence',
+	        send_location_by_email: 'Send location by Email',
+	        send_location_url_by_email: 'Send location URL by Email',
+	        send_location_by_sms: 'Send location by SMS',
+	        send_location_url_by_sms: 'Send location URL by SMS'
+	    },
     en: {
         _name_: 'English',
         // login screen
