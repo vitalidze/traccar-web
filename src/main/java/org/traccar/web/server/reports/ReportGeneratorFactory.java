ecfb5cde0376453378608b30e8d66fab3d731f71
leanny <leanny@vega-PUNK>
2017-02-20 14:14:57 +0100

    Change Author name on overall project files..

--- src/main/java/org/traccar/web/server/reports/ReportGeneratorFactory.java
+++ src/main/java/org/traccar/web/server/reports/ReportGeneratorFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Vitaly Litvak (vitavaque@gmail.com)
+ * Copyright 2017 Godwin peter .O (godwin@peter.com.ng)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
